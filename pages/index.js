42c0920bd931995c9f18e8e3b17b99a7301ccf1a
Deividy <dpolicarpo@gmail.com>
2021-05-07 16:56:52 -0300

    first manual rollback

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '/css/dap.module.css'
 function Home(){
     return( 
         <div>
-            <a href="https://wt7.uno" target="_blank">WT7.uno</a>
+            <h1>WT7.uno</h1>
         </div>
     )
 }
